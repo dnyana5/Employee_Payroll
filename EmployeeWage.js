dbd5f16f987c17deee7689d2ff0e6f8c72c24d88
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-02-24 06:47:37 +0530

    Refactored:Code & wrote function for getting  daily working Hours and daily wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,32 +4,33 @@
     const IS_ABSENT = 0;
     let empCheck = Math.floor(Math.random() * 10) % 2;
     if (empCheck == IS_ABSENT) {
-        console.log("UC1 - Employee is ABSENT.Exiting the Program");
+        console.log("Employee is ABSENT.Exiting the Program");
         return;
     } else {
-        console.log("UC1 - Employee is PRESENT");
+        console.log("Employee is PRESENT");
 
     }
 }
-// UC 2 Switch Case
+// UC 3 Function
 const IS_PART_TIME = 0;
 const IS_FULL_TIME = 1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 {
+    function getWorkingHours(empCheck){ 
+        switch(empCheck){
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
     let empHrs = 0;
     let empCheck = Math.floor(Math.random() * 10)% 2;
-    switch(empCheck){
-        case IS_PART_TIME:
-            empHrs = PART_TIME_HOURS;
-            break;
-        case IS_FULL_TIME:
-            empHrs = FULL_TIME_HOURS;
-            break;
-        default:
-            empHrs = 0;
-    }
+    empHrs = getWorkingHours(empCheck);
     let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("UC2 - Employee Wage :" +empWage);
+    console.log("Daily Working Hours : "+empHrs+"\nEmployee Wage : " +empWage);
 }
